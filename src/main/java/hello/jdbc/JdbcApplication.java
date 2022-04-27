160849025cc5db6d1af10ad6c31d9d3d7ce9fd8e
조영혜 <97926463+myjjjjjy@users.noreply.github.com>
2022-04-27 16:56:00 +0900

    프로젝트 생성. lombok, h2, JDBC API Dependencies 추가

--- src/main/java/hello/jdbc/JdbcApplication.java
+++ src/main/java/hello/jdbc/JdbcApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class JdbcApplication {
 
+	// Jdbc 프로젝트 생성
 	public static void main(String[] args) {
 		SpringApplication.run(JdbcApplication.class, args);
 	}
